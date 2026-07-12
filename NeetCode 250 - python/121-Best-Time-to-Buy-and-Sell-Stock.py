d63f593eb6cd9e185f04f8a2d90c202ddc70a994
0Xcybercoder <162822818+0Xcybercoder@users.noreply.github.com>
2026-07-12 17:19:46 +0300

    [121] [Time Beats: 36.59%] [Memory Beats: 7.38%] - LeetPush

---
+++ NeetCode 250 - python/121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        mx = prices[-1]
+        temp = [0] * (len(prices) - 1)
+
+        profit = 0
+        if len(prices) > 2:
+            for i in range(len(temp) - 1, -1, -1):
+                temp[i] = mx
+                if prices[i] > mx:
+                    mx = prices[i]
+        elif len(prices) == 2:
+            profit = prices[1] - prices[0]
+            return profit if profit > 0 else 0
+
+        mx = 0
+        for i in range(len(prices) - 1):
+            tProfit = temp[i] - prices[i]
+
+            if tProfit > mx:
+                profit = tProfit
+                mx = profit
+
+        return profit if profit > 0 else 0
