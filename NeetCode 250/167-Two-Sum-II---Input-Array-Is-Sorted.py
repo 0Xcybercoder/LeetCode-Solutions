35ecd386cb97a8fdb8797ad857177e7d3145c09a
0Xcybercoder <162822818+0Xcybercoder@users.noreply.github.com>
2026-07-12 17:20:45 +0300

    Delete NeetCode 250 directory

--- NeetCode 250/167-Two-Sum-II---Input-Array-Is-Sorted.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-                for num in numbers:
-                    if target - num in numbers:
-                        first = numbers.index(num) + 1
-                        second = numbers.index(target - num) + 1
-                        if first == second:
-                            second += 1
-                        return first, second
